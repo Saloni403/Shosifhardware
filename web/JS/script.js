119daf71d1443291be318702996a0f1f1f1f94be
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-05-13 16:01:40 +0530

    changes done

--- web/JS/script.js
+++ web/JS/script.js
@@ -1,24 +1,55 @@
-let menushow=document.querySelector("#menuicon");
-let menu=document.querySelector(".hiddennav")
-let closing=document.querySelector("#closing")
+let menushow = document.querySelector("#menuicon");
+let menu = document.querySelector(".hiddennav");
+let closing = document.querySelector("#closing");
 
-let Productlistbtn=document.querySelector("#Productlistbtn")
-let ourproducts=document.querySelector(".ourproducts")
-let closinglist=document.querySelector("#closinglist")
+let Productlistbtn = document.querySelector("#Productlistbtn");
+let ourproducts = document.querySelector(".ourproducts");
+let closinglist = document.querySelector("#closinglist");
 
-menushow.addEventListener("click",function(){
-    menu.style.right="0"
-     ourproducts.style.right="-350px"
-})
-closing.addEventListener("click",function(){
-    menu.style.right="-350px"
-   
+let hiddenlist = document.querySelector(".hiddenlist");
+let shopclosing=document.querySelector(".shopclosing");
+
+let quantity = document.querySelector("#value");
+let i = 1;
+let totalquantity = document.querySelector("#qt");
+// totalquantity.innerHTML=i;
+
+// console.log(totalquantity)
+menushow.addEventListener("click", function () {
+  menu.style.right = "0";
+  ourproducts.style.right = "-350px";
+});
+closing.addEventListener("click", function () {
+  menu.style.right = "-350px";
+});
+
+Productlistbtn.addEventListener("click", function () {
+  ourproducts.style.right = "0";
+  menu.style.right = "-350px";
+});
+closinglist.addEventListener("click", function () {
+  ourproducts.style.right = "-350px";
+});
+
+const dots = document.getElementById("threeDots");
+dots.addEventListener("click", () => {
+ 
+  hiddenlist.style.right="0px"
+});
+
+shopclosing.addEventListener("click",()=>{
+     hiddenlist.style.right="-350px";
 })
 
-Productlistbtn.addEventListener("click",function(){
-    ourproducts.style.right="0"
-     menu.style.right="-350px"
-})
-closinglist.addEventListener("click",function(){
-    ourproducts.style.right="-350px"
-})
+function addition() {
+  ++i;
+  quantity.textContent = i;
+  totalquantity.innerHTML = i;
+}
+function deletion() {
+  if (i > 0) {
+    --i;
+    quantity.textContent = i;
+    totalquantity.innerHTML = i;
+  }
+}
