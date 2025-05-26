ddaa382507cec07a471dc7ee49f3c2bc99bb9a0b
Saloni403 <156641454+Saloni403@users.noreply.github.com>
2025-05-26 10:32:08 +0530

    up to date

--- web/JS/script.js
+++ web/JS/script.js
@@ -1,8 +1,5 @@
 let menushow = document.querySelector("#menuicon");
-let menu = document.querySelector(".hiddennav");
-let closing = document.querySelector("#closing");
 
-let Productlistbtn = document.querySelector("#Productlistbtn");
 let ourproducts = document.querySelector(".ourproducts");
 let closinglist = document.querySelector("#closinglist");
 
@@ -12,20 +9,9 @@ let shopclosing=document.querySelector(".shopclosing");
 let quantity = document.querySelector("#value");
 let i = 1;
 let totalquantity = document.querySelector("#qt");
-// totalquantity.innerHTML=i;
 
-// console.log(totalquantity)
 menushow.addEventListener("click", function () {
-  menu.style.right = "0";
-  ourproducts.style.right = "-350px";
-});
-closing.addEventListener("click", function () {
-  menu.style.right = "-350px";
-});
-
-Productlistbtn.addEventListener("click", function () {
   ourproducts.style.right = "0";
-  menu.style.right = "-350px";
 });
 closinglist.addEventListener("click", function () {
   ourproducts.style.right = "-350px";
